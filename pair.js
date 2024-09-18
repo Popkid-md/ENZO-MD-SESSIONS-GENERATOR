6665e6ff763e2e14ab461ff77d68b400841c94cc
Popkid-md <weanslatevanessa@gmail.com>
2024-09-18 13:00:23 +0300

    Popkid😈

--- pair.js
+++ pair.js
@@ -58,19 +58,19 @@ router.get('/', async (req, res) => {
 
                let GIFTED_MD_TEXT = `
 ┏━━━━━━━━━━━━━━
-┃𝙴𝙽𝚉𝙾 𝙼𝙳 𝑆𝑒𝑠𝑠𝑖𝑜𝑛
+┃POPKID 𝙼𝙳 𝑆𝑒𝑠𝑠𝑖𝑜𝑛
 ┃𝑆𝑢𝑐𝑐𝑒𝑠𝑓𝑢𝑙𝑙𝑦
 ┃𝐶𝑜𝑛𝑛𝑒𝑐𝑡𝑒𝑑
 ┗━━━━━━━━━━━━━━━
 ▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-❶ || 𝐶𝑟𝑒𝑎𝑡𝑜𝑟 = 𝙴𝙽𝚉𝙾_𝚃𝙴𝙲𝙷
+❶ || 𝐶𝑟𝑒𝑎𝑡𝑜𝑟 = POPKID_𝚃𝙴𝙲𝙷
 ▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-❷ || YouTube Channel = https://Instagram.com/@_BERA_TECH?si=3cp6Y2r__cXZg-Wi
+❷ || YouTube Channel = https://whatsapp.com/channel/0029VadQrNI8KMqo79BiHr3l
 ▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-Please Follow My Support Channel https://whatsapp.com/channel/0029VajJTJp2f3ELCm8FN50D
-Wanna talk? http://wa.me/254784115308?
+Please Follow My Support Channel https://whatsapp.com/channel/0029VadQrNI8KMqo79BiHr3l
+Wanna talk? http://wa.me/254111385747?
 ▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-©*𝙴𝙽𝚉𝙾 𝐴 𝑃𝑟𝑜 𝐷𝑒𝑣𝑒𝑙𝑜𝑝𝑒𝑟*
+©*POPKID YOUR CORDER🌟*
 
 _Don't Forget To Give Star To My Repo_`
  await Pair_Code_By_Gifted_Tech.sendMessage(Pair_Code_By_Gifted_Tech.user.id,{text:GIFTED_MD_TEXT},{quoted:session})
